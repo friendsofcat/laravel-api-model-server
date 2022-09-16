bcd2c13e202cb7bbfe530a28a55a9b32a22f93a2
David Matta <matta.davi@gmail.com>
2022-09-16 15:10:25 +0200

    Better error handling and ensure only allowed attributes (and aliases) are returned

--- src/ApiRequest.php
+++ src/ApiRequest.php
@@ -3,7 +3,10 @@
 namespace FriendsOfCat\LaravelApiModelServer;
 
 use RuntimeException;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use FriendsOfCat\LaravelApiModelServer\Rules\SortRule;
 use FriendsOfCat\LaravelApiModelServer\Rules\FieldsRule;
 use FriendsOfCat\LaravelApiModelServer\Rules\FilterRule;
@@ -15,8 +18,6 @@ use FriendsOfCat\LaravelApiModelServer\Rules\SelectRawRule;
 
 abstract class ApiRequest extends FormRequest
 {
-    protected $stopOnFirstFailure = true;
-
     /*
      * Full namespace of Api model schema class.
      */
@@ -89,5 +90,80 @@ abstract class ApiRequest extends FormRequest
             ],
             //            'compressed' => '',
         ];
+    }
+
+    /**
+     * Prepare the data for validation.
+     * We should retrieve only allowed attributes as defined in Schema.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        if ($this->shouldOverwriteFields()) {
+            $this->merge([
+                'fields' => $this->setDefaultFields(),
+            ]);
+        }
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
+    }
+
+    protected function shouldOverwriteFields(): bool
+    {
+        if (! isset($this->fields) && $this->getSchema()->getAllowedAttributes() != 'all') {
+            return true;
+        }
+
+        $fields = explode($this->getSchema()->getParser()::ARRAY_VALUE_SEPARATOR, $this->fields);
+
+        return ! empty(array_filter(
+            $fields,
+            fn ($field) => str_contains($field, '*')
+        ));
+    }
+
+    protected function setDefaultFields()
+    {
+        $allowedAttributes = $this->getSchema()->getAllowedAttributes();
+
+        $fields = $allowedAttributes['values'];
+
+        // To have the ability to include everything but restricted columns,
+        // we need to retrieve all column names from DB and exclude restricted columns.
+        // NOTE: This will create an extra query. Result will be cached for 24h.
+        if ($allowedAttributes['mode'] == 'restricted') {
+            $model = $this->getSchema()->getModel();
+            $key = 'laravel-api-model-server|' . $model->getConnection()->getDatabaseName() . '|columns';
+            $modelColumns = Cache::remember(
+                $key,
+                60 * 60 * 24,
+                fn () => $model->getConnection()->getSchemaBuilder()->getColumnListing($model->getTable())
+            );
+
+            $fields = array_filter(
+                $modelColumns,
+                fn ($column) => ! in_array($column, $allowedAttributes['values'])
+            );
+        }
+
+        // Include aliased columns in response
+        foreach ($this->getSchema()->getAttributeAliases() as $alias => $column) {
+            $fields[] = $alias;
+        }
+
+        // If request wants to retrieve some custom fields (like aliased columns) but also everything else,
+        // we need to overwrite this request to retrieve only allowed attributes defined within schema.
+        if (isset($this->fields)) {
+            $fields = array_merge($fields, array_filter(
+                explode($this->getSchema()->getParser()::ARRAY_VALUE_SEPARATOR, $this->fields),
+                fn ($field) => ! str_contains($field, '*')
+            ));
+        }
+
+        return implode($this->getSchema()->getParser()::ARRAY_VALUE_SEPARATOR, $fields);
     }
 }
